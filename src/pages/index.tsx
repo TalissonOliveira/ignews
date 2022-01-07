8ef2b901fdf21e6271bdc419bf867df15a6c12db
Talisson Oliveira <talissonlux@outlook.com>
2022-01-07 17:23:23 -0300

    feat: create home page component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,31 @@
 import Head from 'next/head'
 
+import styles from './home.module.scss'
+
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Início | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
 
-      <h1>Hello</h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>
+            <span>👋</span> Hey, welcome
+          </span>
+          <h1>
+            News about <br /> the <span>React</span> world
+          </h1>
+
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   )
 }
