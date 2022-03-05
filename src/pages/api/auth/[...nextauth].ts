946b0b04c1768e501e75d19d24fb8c0e0b57ceff
Talisson Oliveira <talissonlux@outlook.com>
2022-03-04 23:35:46 -0300

    feat: save user in database

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
 import NextAuth from 'next-auth'
 import GithubProvider from 'next-auth/providers/github'
+import { query as q } from 'faunadb'
+
+import { fauna } from '../../../services/fauna'
 
 export default NextAuth({
   providers: [
@@ -12,5 +15,23 @@ export default NextAuth({
         }
       }
     })
-  ]
+  ],
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const { email } = user
+
+      try {
+        await fauna.query(
+          q.Create(
+            q.Collection('users'),
+            { data: { email }}
+          )
+        )
+
+        return true
+      } catch {
+        return false
+      }
+    }
+  }
 })
