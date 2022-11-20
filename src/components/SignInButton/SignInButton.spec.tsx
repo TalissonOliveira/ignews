96897259c073e0933bafec51a1678c45592ea253
Talisson Oliveira <talissonlux@outlook.com>
2022-11-20 02:42:45 -0300

    test: creates tests for the SignInButton component

---
+++ src/components/SignInButton/SignInButton.spec.tsx
@@ -0,0 +1,40 @@
+import { render, screen } from '@testing-library/react'
+import { mocked } from 'jest-mock'
+import { useSession } from 'next-auth/react'
+import { SignInButton } from '.'
+
+jest.mock('next-auth/react')
+
+describe('SignInButton component', () => {
+  it('renders correctly when user is not authenticated', () => {
+    const useSessionMocked = mocked(useSession) // or jest.mocked()
+
+    useSessionMocked.mockReturnValueOnce({
+      data: null,
+      status: "unauthenticated"
+    })
+
+    render(<SignInButton />)
+
+    expect(screen.getByText('Sign in with Github')).toBeInTheDocument()
+  })
+
+  it('renders correctly when user is authenticated', () => {
+    const useSessionMocked = mocked(useSession)
+
+    useSessionMocked.mockReturnValueOnce({
+      data: {
+        user: {
+          name: 'John Doe',
+          email: 'johndoe@example.com'
+        },
+        expires: 'fake-expires'
+      },
+      status: "authenticated"
+    })
+
+    render(<SignInButton />)
+
+    expect(screen.getByText('John Doe')).toBeInTheDocument()
+  })
+})
