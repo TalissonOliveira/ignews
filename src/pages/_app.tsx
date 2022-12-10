662991419e9b86db36517739a651ee3c5ceb8acb
Talisson Oliveira <talissonlux@outlook.com>
2022-12-10 01:23:30 -0300

    fix: fixes pageProps typing for next auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,13 @@
+import { Session } from 'next-auth'
 import { SessionProvider } from 'next-auth/react'
 import { AppProps } from 'next/app'
 import { Header } from '../components/Header'
 
 import '../styles/global.scss'
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }: AppProps<{
+  session: Session
+}>) {
   return (
     <>
       <SessionProvider session={pageProps.session}>
