9927bb0d5cac8008b4dbdd3d0fcf24c507b4e428
Talisson Oliveira <talissonlux@outlook.com>
2022-12-10 01:29:40 -0300

    feat: adds secret in next auth settings

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -5,6 +5,7 @@ import { query as q } from 'faunadb'
 import { fauna } from '../../../services/fauna'
 
 export default NextAuth({
+  secret: process.env.NEXTAUTH_SECRET,
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID,
